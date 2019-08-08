cd9692eda3a72b88b799873cf32b7aedb1f9811c
WeiChaoChao <17687910227@163.com>
2019-08-08 13:59:03 +0800

    springboot整合redis

---
+++ src/test/java/com/example/demo/redis/redisTest.java
@@ -0,0 +1,28 @@
+package com.example.demo.redis;
+
+import com.example.demo.enity.UserEnity;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class redisTest {
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Test
+    public void setValue() {
+        UserEnity userEnity = new UserEnity();
+        userEnity.setName("chaochao");
+        userEnity.setAge(23);
+        userEnity.setId(10L);
+        redisTemplate.opsForValue().set("student", userEnity);
+    }
+}
