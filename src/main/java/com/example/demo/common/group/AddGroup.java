74ab8debf24bbe776fd11360199eb5b7fd58ca0a
WeiChaoChao <17687910227@163.com>
2019-08-09 13:38:02 +0800

    参数校验代码提交

---
+++ src/main/java/com/example/demo/common/group/AddGroup.java
@@ -0,0 +1,11 @@
+package com.example.demo.common.group;
+
+/**
+ * 新增分组
+ *
+ * @Author: WeiChaoChao
+ * @Email: 17687910227@163.com
+ * @Date: 2019/8/9
+ */
+public interface AddGroup {
+}
---
+++ src/main/java/com/example/demo/common/group/UpdateGroup.java
@@ -0,0 +1,9 @@
+package com.example.demo.common.group;
+/**
+ * 更新分组
+* @Author: WeiChaoChao
+* @Email: 17687910227@163.com
+* @Date: 2019/8/9
+*/
+public interface UpdateGroup {
+}
